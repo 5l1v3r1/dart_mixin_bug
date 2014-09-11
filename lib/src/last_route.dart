a889f384cf3118f9a4c92debd3c59901fc455eb3
Alex Nichol <unixpickle@gmail.com>
2014-09-11 18:15:51 -0400

    stripped LastRoute

--- lib/src/last_route.dart
+++ lib/src/last_route.dart
@@ -1,25 +1,11 @@
 part of web_router;
 
-/**
- * A method which is called to handle a [RouteRequest].
- */
 typedef void LastRouteFunction(RouteRequest req);
 
-/**
- * A class intended to be used as a mixin to handle requests with an anonymous
- * function
- */
 class LastRoute {
-  /**
-   * The function to use as the handler.
-   */
   LastRouteFunction handlerFunction;
   
-  /**
-   * Runs [handlerFunction] and returns a [Future] that completes with `false`.
-   */
   Future<bool> handle(RouteRequest request) {
-    handlerFunction(request);
     return new Future(() => false);
   }
 }
