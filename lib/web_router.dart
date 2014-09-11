e573e88512f31fca16e4a70039959655f8686fc3
Alex Nichol <unixpickle@gmail.com>
2014-09-11 18:23:24 -0400

    cleanup web_router.dart

--- lib/web_router.dart
+++ lib/web_router.dart
@@ -1,30 +1,4 @@
-/**
- * A simple and expandable way to route HTTP requests.
- * 
- * The [Router] class has a method, `httpHandler`, specifically for processing
- * [HttpRequest]s straight from an [HttpServer]. The [Router] runs these
- * through a series of [Route]s. Each [Route] may record information about the
- * request before passing it to the next [Route], or it may choose to respond to
- * the request and prevent further [Route]s from being invoked.
- * 
- * The [Router] provides additional utility methods like `get` and `post` to
- * make it super easy to register simple handler functions in one line of code.
- * Here is an example of how you might configure a simple [Router] to serve one
- * page, "a.txt":
- * 
- *     Router router = new Router();
- *     router.get('/a.txt', (RouteRequest r) {
- *       return r.response..write('hey')..close();
- *     });
- *     HttpServer.bind('localhost', 1337).then((HttpServer server) {
- *       server.listen(router.httpHandler);
- *     });
- *
- */
 library web_router;
-
-import 'dart:io';
-import 'dart:async';
 
 part 'src/router.dart';
 part 'src/path_route.dart';
